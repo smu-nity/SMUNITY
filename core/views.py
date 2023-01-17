5b9920e4c7ac0920882d98f678388b412d12e5b0
gomin0 <gomin0981113@naver.com>
2023-01-17 17:43:28 +0900

    feat: 마이페이지 (#24)

--- core/views.py
+++ core/views.py
@@ -1,10 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+
+from accounts.models import Profile
+from core.models import Course
+from graduations.models import Subject
+
 
 def home(request):
     return render(request, 'core/head.html')
 
 
 def mypage(request):
-    return render(request, 'core/mypage.html')
+    user = request.user
+    profile = Profile.objects.get(user=user)
+    cg_list = Course.objects.filter(user=user)
+    return render(request, 'core/mypage.html', {'user': user, 'profile': profile, 'cg_list': cg_list})
 
 
