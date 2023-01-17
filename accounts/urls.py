a8203dbbfda1436f45154c82fffa5fbdce7bc09b
hyunmin0317 <choihm9903@naver.com>
2023-01-17 16:24:46 +0900

    feat: 비밀번호 변경 세팅 (#25)

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
     path('agree/', views.agree, name='agree'),
-    path('change/', views.change_pw, name='change'),
+    path('change/<str:user>/', views.change_pw, name='change'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/head.html')
 
 
+@login_required
 def mypage(request):
     return render(request, 'core/mypage.html')
 
 
+@login_required
 def custom(request):
     return render(request, 'core/custom.html')
