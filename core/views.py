2831e4a397ccd7c43ca7dcba7c71f704187b8b73
jiiiiiw <kijiwi1@naver.com>
2023-01-17 18:26:27 +0900

    feat: 삭제기능 확인부탁(#23)

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
+from pyexpat.errors import messages
+
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Course
 
@@ -17,3 +19,12 @@ def custom(request):
     courses = Course.objects.filter(user=request.user)
     context = {'courses':courses}
     return render(request, 'core/custom.html', context)
+
+@login_required
+def course_delete(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    if request.user != course.user:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('core:custom', course_id=course.subject.number)
+    course.delete()
+    return redirect('core:custom')
