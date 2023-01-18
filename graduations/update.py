d3030f087148c49183a684eb546d60bf2f235fab
hyunmin0317 <choihm9903@naver.com>
2023-01-19 00:17:46 +0900

    feat: 컴퓨터과학과 전공 과목 추가 (#36)

--- graduations/update.py
+++ graduations/update.py
@@ -1,11 +1,13 @@
 # 장고 환경
 import os
+import json
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 application = get_wsgi_application()
 
-import json
-from graduations.models import Subject
+from graduations.models import Subject, Major
+from accounts.models import Department
 
 
 def main(year, semester):
@@ -18,8 +20,14 @@ def main(year, semester):
             if not Subject.objects.filter(number=SBJ_NO):
                 Subject.objects.create(number=SBJ_NO, name=data['SBJ_NM'], credit=data['CDT'], dept=data['EST_DEPT_INFO'], type=data['CMP_DIV_NM'])
 
+def major(dept, subjects):
+    for subject in subjects:
+        Major.objects.create(department=dept, subject=subject, type=subject.type)
+
 
 if __name__ == '__main__':
-    data = [['2018', '10'], ['2018', '11'], ['2018', '20'], ['2018', '21'], ['2019', '10'], ['2019', '11'], ['2019', '20'], ['2019', '21'], ['2020', '10'], ['2020', '11'], ['2020', '20'], ['2020', '21'], ['2021', '10'], ['2021', '11'], ['2021', '20'], ['2021', '21'], ['2022', '10'], ['2022', '11'], ['2022', '20'], ['2022', '21'], ['2023', '10']]
-    for d in data:
-        main(d[0], d[1])
+    # data = [['2018', '10'], ['2018', '11'], ['2018', '20'], ['2018', '21'], ['2019', '10'], ['2019', '11'], ['2019', '20'], ['2019', '21'], ['2020', '10'], ['2020', '11'], ['2020', '20'], ['2020', '21'], ['2021', '10'], ['2021', '11'], ['2021', '20'], ['2021', '21'], ['2022', '10'], ['2022', '11'], ['2022', '20'], ['2022', '21'], ['2023', '10']]
+    # for d in data:
+    #     main(d[0], d[1])
+    dept, subjects = Department.objects.get(name='컴퓨터과학전공'), Subject.objects.filter(dept__contains='컴퓨터과학')
+    major(dept, subjects)
