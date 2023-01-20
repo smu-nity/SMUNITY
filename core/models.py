98902c30c2fb1bc984e48b00c230082710522a6f
hyunmin0317 <choihm9903@naver.com>
2023-01-20 16:59:06 +0900

    feat: 모델 설명 (#50)

--- core/models.py
+++ core/models.py
@@ -4,14 +4,14 @@ from config.settings import YEAR_CHOICES
 from graduations.models import Subject
 
 
-class Course(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    year = models.CharField(max_length=4, choices=YEAR_CHOICES)
-    semester = models.CharField(max_length=20)
-    credit = models.IntegerField()
-    type = models.CharField(max_length=3)
-    domain = models.CharField(max_length=20, null=True, blank=True)
+class Course(models.Model):     # 기이수과목 테이블
+    user = models.ForeignKey(User, on_delete=models.CASCADE)        # 유저
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)  # 과목
+    year = models.CharField(max_length=4, choices=YEAR_CHOICES)     # 이수 연도
+    semester = models.CharField(max_length=20)  # 이수 학기
+    credit = models.IntegerField()              # 학점
+    type = models.CharField(max_length=3)       # 이수 구분
+    domain = models.CharField(max_length=20, null=True, blank=True) # 교양 영역명
 
     def __str__(self):
         return f'[{self.user}] {self.subject}'
--- graduations/models.py
+++ graduations/models.py
@@ -4,31 +4,31 @@ from accounts.models import Department
 from config.settings import TYPE_CHOICES, SUBTYPE_CHOICES_E, SUBTYPE_CHOICES_S
 
 
-class Subject(models.Model):
-    number = models.CharField(max_length=10, unique=True)
-    name = models.CharField(max_length=30)
-    credit = models.IntegerField()
-    dept = models.CharField(max_length=30)
-    type = models.CharField(max_length=3, choices=TYPE_CHOICES)
+class Subject(models.Model):    # 과목 테이블
+    number = models.CharField(max_length=10, unique=True)       # 학수번호
+    name = models.CharField(max_length=30)  # 이름
+    credit = models.IntegerField()          # 학점
+    dept = models.CharField(max_length=30)  # 개설학부
+    type = models.CharField(max_length=3, choices=TYPE_CHOICES) # 이수구분
 
     def __str__(self):
         return f'[{self.number}] {self.name}'
 
 
-class Major(models.Model):
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    type = models.CharField(max_length=3, choices=(('1전선', '1전선'), ('1전심', '1전심')))
+class Major(models.Model):      # 전공 과목 테이블
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)    # 학과
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)          # 과목
+    type = models.CharField(max_length=3, choices=(('1전선', '1전선'), ('1전심', '1전심')))  # 이수구분
 
     def __str__(self):
         return f'[{self.department}] {self.subject}'
 
 
-class Culture(models.Model):
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    type = models.CharField(max_length=2, choices=(('교필', '교필'), ('교선', '교선')))
-    domain = models.CharField(max_length=2, choices=(('핵심', '핵심'), ('기초', '기초'), ('균형', '균형'), ('일반', '일반')))
-    subdomain = models.CharField(max_length=20, choices=SUBTYPE_CHOICES_E+SUBTYPE_CHOICES_S)
+class Culture(models.Model):    # 교양 과목 테이블
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)                     # 과목
+    type = models.CharField(max_length=2, choices=(('교필', '교필'), ('교선', '교선')))     # 이수구분
+    domain = models.CharField(max_length=2, choices=(('핵심', '핵심'), ('기초', '기초'), ('균형', '균형'), ('일반', '일반')))  # 영역명
+    subdomain = models.CharField(max_length=20, choices=SUBTYPE_CHOICES_E+SUBTYPE_CHOICES_S)    # 세부 영역명
 
     def __str__(self):
         return f'[{self.type}] {self.subject}'
