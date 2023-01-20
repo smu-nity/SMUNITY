abcfd9cb6b4abdd7102254faeb93bc49fe300290
hyunmin0317 <choihm9903@naver.com>
2023-01-20 21:39:36 +0900

    fix: NoneType Error (#52)

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -5,12 +5,24 @@ register = template.Library()
 
 @register.filter
 def sub(a, b):
+    if not b:
+        b = 0
     if b >= a:
         return 0
     return a - b
 
+
 @register.filter
 def div(a, b):
+    if not a:
+        a = 0
     if a >= b:
         return 100
     return int(a / b * 100)
+
+
+@register.filter
+def zero_filter(a):
+    if a:
+        return a
+    return 0
