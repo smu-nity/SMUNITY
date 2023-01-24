1a961803cef322e65c9fe9638849766239df35bd
hyunmin0317 <choihm9903@naver.com>
2023-01-24 13:16:19 +0900

    feat: 데이터 업데이트 스크립트 (#66)

--- graduations/update.py
+++ graduations/update.py
@@ -3,13 +3,12 @@ import os
 import json
 from django.core.wsgi import get_wsgi_application
 
-from accounts.models import Department
-from config.settings import SUBTYPE_CHOICES_S, SUBTYPE_CHOICES_E
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 application = get_wsgi_application()
 
+from accounts.models import Department, Year
 from graduations.models import Subject, Major, Culture
+from config.settings import SUBTYPE_CHOICES_S, SUBTYPE_CHOICES_E
 
 
 # 과목 업데이트 스크립트
@@ -23,10 +22,41 @@ def subjects(year, semester):
             if not Subject.objects.filter(number=SBJ_NO):
                 Subject.objects.create(number=SBJ_NO, name=data['SBJ_NM'], credit=data['CDT'], dept=data['EST_DEPT_INFO'], type=data['CMP_DIV_NM'])
 
+
 # 전공 과목 업데이트 스크립트
-def major(dept, subjects, type):
+def major(dept, subjects):
     for subject in subjects:
-        Major.objects.create(department=dept, subject=subject, type=type)
+        numbers = subject['numbers']
+        for number in numbers:
+            try:
+                sub = Subject.objects.get(number=number)
+                Major.objects.create(department=dept, subject=sub, grade=subject['grade'], semester=subject['semester'], type=subject['type'])
+            except:
+                print(number)
+
+
+# 학년도 업데이트 스크립트
+def year():
+    datas = [
+        {'year': '2017', 'major_i': 15, 'major_s': 45, 'culture': 36, 'culture_cnt': 4, 'all': 130},
+        {'year': '2018', 'major_i': 15, 'major_s': 45, 'culture': 33, 'culture_cnt': 4, 'all': 130},
+        {'year': '2019', 'major_i': 15, 'major_s': 45, 'culture': 33, 'culture_cnt': 4, 'all': 130},
+        {'year': '2020', 'major_i': 15, 'major_s': 45, 'culture': 33, 'culture_cnt': 5, 'all': 130},
+        {'year': '2021', 'major_i': 15, 'major_s': 45, 'culture': 33, 'culture_cnt': 5, 'all': 130},
+        {'year': '2022', 'major_i': 15, 'major_s': 45, 'culture': 33, 'culture_cnt': 5, 'all': 130},
+        {'year': '2023', 'major_i': 15, 'major_s': 60, 'culture': 33, 'culture_cnt': 5, 'all': 130}
+    ]
+    for data in datas:
+        Year.objects.create(year=data['year'], major_i=data['major_i'], major_s=data['major_s'], culture=data['culture'], culture_cnt=data['culture_cnt'], all=data['all'])
+
+
+# 학과 업데이트 스크립트
+def departments():
+    datas = [
+        {'college': '융합공과대학', 'name': '컴퓨터과학전공', 'type': '공학', 'call': '02-2287-5292'}
+    ]
+    for data in datas:
+        Department.objects.create(college=data['college'], name=data['name'], type=data['type'], call=data['call'])
 
 
 # 상명핵심역량교양 과목 업데이트 스크립트
@@ -41,8 +71,11 @@ def culture_e():
     for type in types:
         numbers = dic[type]
         for number in numbers:
-            subject = Subject.objects.get(number=number)
-            Culture.objects.create(subject=subject, type='교필', domain='핵심', subdomain=type)
+            try:
+                subject = Subject.objects.get(number=number)
+                Culture.objects.create(subject=subject, type='교필', domain='핵심', subdomain=type)
+            except:
+                print(number)
 
 
 # 균형교양 과목 업데이트 스크립트
@@ -58,20 +91,47 @@ def culture_s():
     for type in types:
         numbers = dic[type]
         for number in numbers:
-            subject = Subject.objects.get(number=number)
-            Culture.objects.create(subject=subject, type='교선', domain='균형', subdomain=type)
+            try:
+                subject = Subject.objects.get(number=number)
+                Culture.objects.create(subject=subject, type='교선', domain='균형', subdomain=type)
+            except:
+                print(number)
+
+
+# 컴퓨터과학과 전공 업데이트 스크립트
+def major_computer_science():
+    dept = Department.objects.get(name='컴퓨터과학전공')
+    subjects = [
+        {'grade': '1학년', 'semester': '1학기', 'type': '1전선', 'numbers': ['HASW0001', 'HAEA9225', 'HAEA9237', 'HAFL7001']},
+        {'grade': '1학년', 'semester': '2학기', 'type': '1전선', 'numbers': ['HASW0002', 'HAEA0032', 'HAEA9226', 'HAFL0012']},
+        {'grade': '2학년', 'semester': '1학기', 'type': '1전선',
+         'numbers': ['HAEA0001', 'HAEA0017', 'HAEA0027', 'HAEA9236', 'HAEA9241', 'HAFL0002']},
+        {'grade': '2학년', 'semester': '2학기', 'type': '1전선',
+         'numbers': ['HAEA0002', 'HAEA0003', 'HAEA0010', 'HAEA9227', 'HAEZ0002', 'HAEZ0004']},
+        {'grade': '3학년', 'semester': '1학기', 'type': '1전선', 'numbers': ['HAEA0005', 'HAEA9239', 'HAEA9243']},
+        {'grade': '3학년', 'semester': '1학기', 'type': '1전심', 'numbers': ['HAEA0004', 'HAEA0008', 'HAEA0012', 'HAEZ0003']},
+        {'grade': '3학년', 'semester': '2학기', 'type': '1전선', 'numbers': ['HAEA0013', 'HAEA9002', 'HAEA9213']},    # HAEA9244
+        {'grade': '3학년', 'semester': '2학기', 'type': '1전심', 'numbers': ['HAEA0011', 'HAEA0014', 'HAEA9228', 'HAGH0030']},
+        {'grade': '4학년', 'semester': '1학기', 'type': '1전선', 'numbers': ['HAEA0020', 'HAEA9229', 'HAEA9240']},
+        {'grade': '4학년', 'semester': '2학기', 'type': '1전선', 'numbers': ['HAEA0026', 'HAEA9231']},
+    ]
+    major(dept, subjects)
+
+
+# 전체 업데이트 스크립트 (17학년도 ~ 22학년도)
+def subjects_all():
+    data = [['2017', '10'], ['2017', '11'], ['2017', '20'], ['2017', '21'], ['2018', '10'], ['2018', '11'],
+            ['2018', '20'], ['2018', '21'], ['2019', '10'], ['2019', '11'], ['2019', '20'], ['2019', '21'],
+            ['2020', '10'], ['2020', '11'], ['2020', '20'], ['2020', '21'], ['2021', '10'], ['2021', '11'],
+            ['2021', '20'], ['2021', '21'], ['2022', '10'], ['2022', '11'], ['2022', '20'], ['2022', '21'], ['2023', '10']]
+    for d in data:
+        subjects(d[0], d[1])
 
 
 if __name__ == '__main__':
-    # data = [['2017', '10'], ['2017', '11'], ['2017', '20'], ['2017', '21'], ['2018', '10'], ['2018', '11'], ['2018', '20'], ['2018', '21'], ['2019', '10'], ['2019', '11'], ['2019', '20'], ['2019', '21'], ['2020', '10'], ['2020', '11'], ['2020', '20'], ['2020', '21'], ['2021', '10'], ['2021', '11'], ['2021', '20'], ['2021', '21'], ['2022', '10'], ['2022', '11'], ['2022', '20'], ['2022', '21'], ['2023', '10']]
-    # for d in data:
-    #     subjects(d[0], d[1])
-
-    numbers = ['HASW0001', 'HAEA9225', 'HAEA9237', 'HAFL7001', 'HAEA0001', 'HAEA0017', 'HAEA0027', 'HAEA9236',
-               'HAEA9241', 'HAFL0002', 'HAEA0005', 'HAEA9239', 'HAEA9243', 'HAEA0020', 'HAEA9229', 'HAEA9240']  # 1전선
-    numbers = ['HAEA0004', 'HAEA0008', 'HAEA0012', 'HAEZ0003']  # 1전심
-    dept, subjects = Department.objects.get(name='컴퓨터과학전공'), Subject.objects.filter(number__in=numbers)
-    major(dept, subjects, '1전심')
-
+    subjects_all()
+    year()
+    departments()
+    major_computer_science()
     culture_e()
-
+    culture_s()
