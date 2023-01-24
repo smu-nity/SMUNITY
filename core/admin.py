23b832dbc2f6941f401f07c3be97f4d2e871e8e8
hyunmin0317 <choihm9903@naver.com>
2023-01-24 14:29:30 +0900

    feat: Course 검색 admin 페이지 (#67)

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from core.models import Course
 
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    search_fields = ['user__username', 'subject__number']
+
+admin.site.register(Course, CourseAdmin)
