ccd854dffccd1b4b76dea25c14ea0921302153ba
hyunmin0317 <choihm9903@naver.com>
2023-01-25 14:43:30 +0900

    feat: Nginx 적용 (#77)

--- config/settings.py
+++ config/settings.py
@@ -124,10 +124,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
