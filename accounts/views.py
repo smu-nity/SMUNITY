aef5a42777ab5dff6d59180ede3dbb7c36527e44
hyunmin0317 <choihm9903@naver.com>
2023-01-28 23:54:56 +0900

    feat: 검사대상표 추가 (#94)

--- accounts/views.py
+++ accounts/views.py
@@ -27,7 +27,9 @@ def agree(request):
             return redirect('accounts:agree')
         messages.error(request, '⚠️ 샘물 포털 ID/PW를 다시 확인하세요! (Caps Lock 확인)')
         return redirect('accounts:agree')
-    return render(request, 'accounts/agree.html')
+    departments = Department.objects.all()
+    dept_num = departments.count()
+    return render(request, 'accounts/agree.html', {'departments': departments, 'dept_num': dept_num})
 
 
 def login(request):
