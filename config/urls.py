7be9fc000ab823d61ed8dde748b7b8684869cfa3
hyunmin0317 <choihm9903@naver.com>
2023-02-10 17:07:21 +0900

    chore: robots.txt 설정 (#163)

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from config import settings
 from core import views
@@ -27,4 +28,5 @@ urlpatterns = [
     path('mypage/', include('core.urls')),
     path('team/', views.team, name='team'),
     path('', views.home, name='home'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
 ]
