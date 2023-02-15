42cce7ec778cd37127fb4b5c87abaa9fb61a2e38
hyunmin0317 <choihm9903@naver.com>
2023-02-15 12:46:32 +0900

    chore: admin 페이지 검색 추가 (#170)

--- graduations/admin.py
+++ graduations/admin.py
@@ -3,9 +3,17 @@ from graduations.models import Subject, Major, Culture
 
 
 class SubjectAdmin(admin.ModelAdmin):
-    search_fields = ['number', 'dept']
+    search_fields = ['number', 'name', 'dept']
+
+
+class MajorAdmin(admin.ModelAdmin):
+    search_fields = ['department__name', 'subject__number', 'subject__name', 'type']
+
+
+class CultureAdmin(admin.ModelAdmin):
+    search_fields = ['subject__number', 'subject__name', 'type', 'domain', 'subdomain']
 
 
 admin.site.register(Subject, SubjectAdmin)
-admin.site.register(Major)
-admin.site.register(Culture)
+admin.site.register(Major, MajorAdmin)
+admin.site.register(Culture, CultureAdmin)
