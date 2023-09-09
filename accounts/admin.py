c25d5bb83ed46c11feb8f4d56842811cb9809755
hyunmin0317 <choihm9903@naver.com>
2023-09-09 10:42:57 +0900

    chore: 관리자 페이지 업데이트 - accounts (#221)

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,22 +4,26 @@ from accounts.models import Department, Year, Profile, LoginHistory, Statistics
 
 class YearAdmin(admin.ModelAdmin):
     search_fields = ['year']
+    list_display = ['year', 'major_i', 'major_s', 'culture', 'culture_cnt', 'all']
 
 
 class DepartmentAdmin(admin.ModelAdmin):
     search_fields = ['college', 'name', 'type']
+    list_display = ['college', 'name', 'type', 'url']
 
 
 class ProfileAdmin(admin.ModelAdmin):
     search_fields = ['name', 'user__username', 'year__year', 'department__name']
-
+    list_display = ['user', 'year', 'department', 'name']
 
 class LoginHistoryAdmin(admin.ModelAdmin):
     search_fields = ['user__username']
+    list_display = ['user', 'login_datetime']
 
 
 class StatisticsAdmin(admin.ModelAdmin):
     search_fields = ['date']
+    list_display = ['date', 'visit_count']
 
 
 admin.site.register(Year, YearAdmin)
