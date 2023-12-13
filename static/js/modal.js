2cfb49fca10587d9ea6df9db4c68e059c9909a0a
hyunmin0317 <choihm9903@naver.com>
2023-12-13 21:25:58 +0900

    feat: navbar 수정 (#248)

--- static/js/modal.js
+++ static/js/modal.js
@@ -10,3 +10,10 @@ var Spanclick = function (x, y) {
         y.style.display = "none";
     }
 }
+
+function logout() {
+    var back_head = confirm('정말 로그아웃 하시겠습니까?');
+    if (back_head) {
+        location.href = '/accounts/logout/';
+    }
+}
