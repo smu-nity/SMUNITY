7327809d56657e63bbb57af7afc374a515d23ce0
hyunmin0317 <choihm9903@naver.com>
2023-12-19 21:28:29 +0900

    chore: ALLOWED HOSTS 수정 (#249)

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-@2^=jof@+(3es+wo!s$i!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', 1))
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', ['*'])
 
 
 # Application definition
