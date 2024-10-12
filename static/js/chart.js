bd843014c5d9adbe2a8a56e3aa6506619352ff55
hyunmin0317 <hyunmin-choi@naver.com>
2024-10-12 21:10:45 +0900

    feat: 프론트 완성 (#316)

---
+++ static/js/chart.js
@@ -0,0 +1,31 @@
+function drawChart() {
+    const visitors = new google.visualization.DataTable();
+    visitors.addColumn('string', '날짜');
+    visitors.addColumn('number', '사용자');
+    visitors.addColumn({type:'number', role:'annotation'})
+    visitors.addRows(visitors_data);
+    const chart = new google.visualization.ColumnChart(document.getElementById('chart_visitors'));
+    chart.draw(visitors, options);
+
+    const members = new google.visualization.DataTable();
+    members.addColumn('date', '날짜');
+    members.addColumn('number', '사용자');
+    members.addRows(members_data);
+    const charts = new google.visualization.LineChart(document.getElementById('chart_members'));
+    charts.draw(members, options);
+
+    window.addEventListener('resize', drawChart, false)
+}
+
+const options = {
+    colors: ['rgb(1, 42, 127)'],
+    vAxis: {
+        minValue: 0
+    },
+    width: '100%',
+    height: 400,
+};
+
+google.charts.load('current', {'packages':['corechart', 'bar']});
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawChart);
